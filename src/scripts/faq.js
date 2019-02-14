f8abd30bb8791c6894ed60bd50ae1687d6aec3f4
vincentvanmin <vincentvm98@hotmail.com>
2019-02-14 15:37:55 +0100

    faq

---
+++ src/scripts/faq.js
@@ -0,0 +1,193 @@
+// Add a "readmore" symbol when clicking on a list item
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
+var question3 = document.getElementById("question3");
+var question4 = document.getElementById("question4");
+var question5 = document.getElementById("question5");
+var question6 = document.getElementById("question6");
+var question7 = document.getElementById("question7");
+var question8 = document.getElementById("question8");
+var question9 = document.getElementById("question9");
+var question10 = document.getElementById("question10");
+var question11 = document.getElementById("question11");
+var question12 = document.getElementById("question12");
+
+function openQuestion1() {
+    question1.classList.add("readmore");
+    question2.classList.remove("readmore");
+    question3.classList.remove("readmore");
+    question4.classList.remove("readmore");
+    question5.classList.remove("readmore");
+    question6.classList.remove("readmore");
+    question7.classList.remove("readmore");
+    question8.classList.remove("readmore");
+    question9.classList.remove("readmore");
+    question10.classList.remove("readmore");
+    question11.classList.remove("readmore");
+    question12.classList.remove("readmore");
+}
+
+function openQuestion2() {
+  question1.classList.remove("readmore");
+  question2.classList.add("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion3() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.add("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion4() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.add("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion5() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.add("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion6() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.add("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion7() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.add("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion8() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.add("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion9() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.add("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion10() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.add("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion11() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.add("readmore");
+  question12.classList.remove("readmore");
+}
+
+function openQuestion12() {
+  question1.classList.remove("readmore");
+  question2.classList.remove("readmore");
+  question3.classList.remove("readmore");
+  question4.classList.remove("readmore");
+  question5.classList.remove("readmore");
+  question6.classList.remove("readmore");
+  question7.classList.remove("readmore");
+  question8.classList.remove("readmore");
+  question9.classList.remove("readmore");
+  question10.classList.remove("readmore");
+  question11.classList.remove("readmore");
+  question12.classList.add("readmore");
+}
